5e58ff60399244efd6baa6755619f3e14a01b7fd
Scott Harvey <scottyharvey33@gmail.com>
2021-05-18 09:16:17 -0600

    main - init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,46 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div v-if="pokemon"
+  class="w-7/12 m-auto bg-purple-100 mt-4 m-20 shadow-2xl flex justify-center flex-col items-center">
+    <h3 class="text-2xl m-auto mt-10 uppercase">{{pokemon.name}}</h3>
+    <div class="flex flex-row flex-1">
+      <img class="w-48" :src="pokemon.sprites.front_shiny" />
+      <img class="w-48" :src="pokemon.sprites.back_shiny" />
+    </div>
+    <h3 class="text-yellow-500 mb-10">Types</h3>
+    <div v-for="(type, idx) in pokemon.types" :key="idx"
+    class="mb-20">
+      <h5 class="text-red-600 uppercase">{{type.type.name}}</h5>
+    </div>
+
   </div>
 </template>
+
+<script>
+import { useRoute } from "vue-router"
+import { reactive, toRefs } from 'vue';
+
+export default {
+
+  setup () {
+    const route = useRoute();
+
+    const state = reactive({
+      pokemon: null,
+    })
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.slug}/`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      state.pokemon = data
+      return data
+    })
+
+    return { ...toRefs(state) }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <!-- {{$route.params.slug}} -->
+  <div class="w-full justify-items-center flex flex-col justify-items-center">
+    <div class="m-auto">
+      <input type="text" class="mb-20 border-blue-400 border-2 p-2 mt-10"
+      v-model='text'
+      placeholder="Search Pokemon"/>
+    </div>
+
+    <div class="text-2x text-blue-400">
+      <div
+      class=" pb10 auto-rows-min"
+      v-for="(pokemon, idx) in filteredPokemon" :key="idx">
+      <router-link :to="`/about/${urlIdLookup[pokemon?.name]}`">
+        {{pokemon.name}}
+      </router-link>
+      </div>
+
+    </div>
+
   </div>
+
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import { reactive, toRefs, computed } from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup(){
+
+    const state = reactive({
+      pokemons:[],
+      urlIdLookup: {},
+      text: '',
+      filteredPokemon:computed(() => updatePokemon())
+    })
+
+    function updatePokemon() {
+      if (!state.text) return []
+
+      return state.pokemons.filter(pokemon =>
+        pokemon.name.includes(state.text)
+      )
+    }
+
+    const offset = 121
+
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=39`)
+    .then(res => res.json())
+    .then(data => {
+      state.pokemons = data.results;
+      state.urlIdLookup = data.results.reduce((all, itm, idx) => {
+        all = { ...all, [itm.name]:idx + 1 + offset}
+        return all
+      },{})
+    })
+
+    return {...toRefs(state)}
   }
 }
 </script>
